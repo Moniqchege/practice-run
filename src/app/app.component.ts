a1ebb9151d2bcd94bee01f00e651b499d76e7f89
MoniqChege <quichege33@gmail.com>
2025-04-09 10:18:21 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,23 +12,8 @@ import { CounterComponent } from "./counter/counter.component";
 export class AppComponent {
   title = 'code step by step with;';
   name = 'Monicah Chege';
-  sayHello() {
-    alert('Hello, from Angular');
-  }
-  username:string = 'Monicah Chege';
-  age: number | string = 25;
-  email='abc@gmail.com';
-  contact=1234567890;
-  displayVal = '';
-
-  apple() {
-    this.age='abc'
-  }
-  getData(val:string){
-    console.warn(val);
-  }
-  getValue(val:string){
-    console.warn(val);
-    this.displayVal=val;
-  }
+  imageUrl = 'https://angular.io/assets/images/logos/angular/angular.png';
+  isDisabled = true;
+  
+  
 }
